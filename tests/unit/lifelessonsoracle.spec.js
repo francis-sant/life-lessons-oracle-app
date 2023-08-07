145872cae8a15ca3a42556fc92862eb6bf24b02c
Francisco <2295460@edu.vaniercollege.qc.ca>
2023-08-06 23:57:15 -0400

    More tests descriptions added

--- tests/unit/lifelessonsoracle.spec.js
+++ tests/unit/lifelessonsoracle.spec.js
@@ -10,11 +10,11 @@ describe("LifeLessonsOracle.vue", () => {
     const wrapper = shallowMount(LifeLessonsOracle, {});
   });
 
-  it("displays life lessons category in a list", async () => {
+  it("displays life lessons category in a list", () => {
     
   });
 
-  it("does not display the clear button when life lessons array is empty", async () => {
+  it("does not display the clear button when life lessons array is empty", () => {
 
   });
 
@@ -22,9 +22,39 @@ describe("LifeLessonsOracle.vue", () => {
 
   });
 
-  it("updates lessons array with random life lessons on Random category", async () => {
+  it("does display the learn My lesson button when category is selected", () => {
+
+  });
+
+  it("updates lessons array with random life lessons on Random category",  () => {
     
   });
 
+  it("does not show the card lesson while category is been selected",  () => {
+    
+  });
 
+  it("does not display Like, Share, and Save buttons when card is been selected",  () => {
+
+  });
+
+  it("does show the My Card Lesson when the button Display My Lesson is clicked",  () => {
+    
+  });
+
+  it("does display the clear button when life lessons card is shown",  () => {
+
+  });
+
+  it("does display Like, Share, and Save buttons when card is shown", () => {
+
+  });
+
+  it("does Save button save in persons profile", () => {
+
+  });
+
+  it("does Like button works properly", () => {
+
+  });
 });
